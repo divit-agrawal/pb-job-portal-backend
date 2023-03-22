61eea1fb7c48a489042e6726ca5f08cf8234b817
divit-agrawal <divitagrawal@gmail.com>
2023-03-22 11:16:33 +0530

    fixed get route

--- routes/getJobs.js
+++ routes/getJobs.js
@@ -4,13 +4,25 @@ const router = express.Router();
 
 const Job = require("../models/job");
 
-const verify = require("./verifyMiddleware");
+const User = require("../models/user");
 
 router.get("/", async (req, res) => {
-  // console.log(req.body);
   const jobs = await Job.find();
-  console.log(jobs);
-  res.send(jobs);
+  let arr = [];
+  for (let i = 0; i < jobs.length; i++) {
+    const job = jobs[i];
+    const user = await User.findOne({ email: job.email });
+    // job.username = user.username;
+    // console.log(job);
+    arr.push({
+      id: job.id,
+      email: job.email,
+      jd: job.jd,
+      username: user.username,
+    });
+    // console.log(arr[i]);
+  }
+  res.send(arr);
 });
 
 module.exports = router;
